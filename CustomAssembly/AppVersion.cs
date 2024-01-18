bc3194067852ad9c1573a0d28dd83d59e27b9081
tomaspr <tomaspr@WINDOWS-DTJ4FN3>
2024-01-18 20:36:12 +0100

    Přidejte soubory projektu.

---
+++ CustomAssembly/AppVersion.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace CustomAssembly
+{
+    public class AppVersion
+    {
+        public Version VersionParse(string version)
+        {
+            try
+            {
+                var parsedVersion = Version.Parse(version);
+                return parsedVersion;
+            }
+            catch (FormatException)
+            {
+                // Při neplatném formátu vrátí null
+                return null;
+            }
+        }
+
+        public VersionStatus CompareVersions(string Local , string Server)
+        {
+            var parsedVersion1 = VersionParse(Local);
+            var parsedVersion2 = VersionParse(Server);
+
+            if (parsedVersion1 == null || parsedVersion2 == null)
+            {
+                // Pokud verze nelze parsovat, vrátí Status s hodnotou Invalid
+                return VersionStatus.Invalid;
+            }
+
+            var comparisonResult = parsedVersion1.CompareTo(parsedVersion2);
+
+            if (comparisonResult > 0)
+            {
+                return VersionStatus.New;
+            }
+            else if (comparisonResult < 0)
+            {
+                return VersionStatus.Old;
+            }
+            else
+            {
+                return VersionStatus.Same;
+            }
+        }
+
+        public enum VersionStatus
+        {
+            New,
+            Old,
+            Same,
+            Invalid // Přidána hodnota pro neplatné verze
+        }
+    }
+}
---
+++ CustomAssembly/Class1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace CustomAssembly
+    {
+    public class CustomAssembly
+        {
+            private readonly Assembly _assembly;
+            public CustomAssembly(Assembly assembly)
+            {
+                _assembly = assembly;
+            }
+            public string GetTitle()
+            {
+                var version = _assembly.GetName().Version;
+                var titleAttribute = (AssemblyTitleAttribute)Attribute.GetCustomAttribute(_assembly, typeof(AssemblyTitleAttribute));
+
+                return $"{titleAttribute?.Title} - {version}";
+            }
+
+            public CustomAssemblyOutput GetInfo( )
+            {
+            var customAssembly = new CustomAssemblyOutput();
+                customAssembly.Version = _assembly.GetName().Version;
+                customAssembly.TitleAttribute = (AssemblyTitleAttribute)Attribute.GetCustomAttribute(_assembly, typeof(AssemblyTitleAttribute));
+                customAssembly.DescriptionAttribute = (AssemblyDescriptionAttribute)Attribute.GetCustomAttribute(_assembly, typeof(AssemblyDescriptionAttribute));
+                customAssembly.CompanyAttribute = (AssemblyCompanyAttribute)Attribute.GetCustomAttribute(_assembly, typeof(AssemblyCompanyAttribute));
+                customAssembly.ProductAttribute = (AssemblyProductAttribute)Attribute.GetCustomAttribute(_assembly, typeof(AssemblyProductAttribute));
+
+                return customAssembly;
+            }
+
+            public CustomAssemblyStringOutput GetInfoString( )
+            {
+            var customAssembly = new CustomAssemblyStringOutput();
+                customAssembly.Version = _assembly.GetName( ).Version?.ToString();
+                customAssembly.TitleAttribute = ((AssemblyTitleAttribute)Attribute.GetCustomAttribute(_assembly , typeof(AssemblyTitleAttribute)))?.Title;
+                customAssembly.DescriptionAttribute = Attribute.GetCustomAttribute(_assembly , typeof(AssemblyDescriptionAttribute))?.ToString();
+                customAssembly.CompanyAttribute = ((AssemblyCompanyAttribute)Attribute.GetCustomAttribute(_assembly , typeof(AssemblyCompanyAttribute)))?.Company;
+                customAssembly.ProductAttribute = ((AssemblyProductAttribute)Attribute.GetCustomAttribute(_assembly , typeof(AssemblyProductAttribute)))?.Product;
+
+                return customAssembly;
+            }
+
+        public async Task<string> SaveToJsonFile(string filePath)
+        {
+            try
+            {
+                var json = JsonSerializer.Serialize(GetInfoString());
+                await File.WriteAllTextAsync(filePath, json);
+                return String.Empty;
+            }
+            catch(Exception ss)
+            {
+                return ss.Message;
+            }
+        }
+
+        public CustomAssemblyStringOutput LoadFromJsonFile(string filePath, out string Err)
+        {
+            try
+            {
+                var json = File.ReadAllText(filePath);
+                Err = string.Empty;
+                return JsonSerializer.Deserialize<CustomAssemblyStringOutput>(json);
+            }
+            catch (Exception ss)
+            {
+                Err =ss.Message;
+                return null;
+            }
+            }
+        }
+
+        public class CustomAssemblyOutput
+        {
+            public Version Version { get; set; }
+            public AssemblyTitleAttribute TitleAttribute { get; set; }
+            public AssemblyDescriptionAttribute DescriptionAttribute { get; set; }
+            public AssemblyCompanyAttribute CompanyAttribute { get; set; }
+            public AssemblyProductAttribute ProductAttribute { get; set; }
+        }
+        public class CustomAssemblyStringOutput
+        {
+            public string Version { get; set; }
+            public string TitleAttribute { get; set; }
+            public string DescriptionAttribute { get; set; }
+            public string CompanyAttribute { get; set; }
+            public string ProductAttribute { get; set; }
+        }
+    }
